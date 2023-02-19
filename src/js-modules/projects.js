ed40f72432122e3249b2661ff5ea3026ee837c4a
Carson <carsontwj@gmail.com>
2023-02-19 10:40:28 -0500

    Create basic functions to modify array

--- src/js-modules/projects.js
+++ src/js-modules/projects.js
@@ -10,4 +10,11 @@ function getTitles () {
     return projectTitles;
 }
 
-export {addTitle, getTitles};
+function deleteTitle (title) {
+    const index = projectTitles.indexOf(title);
+    if (index > -1) { // only splice array when item is found
+        projectTitles.splice(index, 1); // 2nd parameter means remove one item only
+    }
+}
+
+export {addTitle, getTitles, deleteTitle};
