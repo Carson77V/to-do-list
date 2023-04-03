e5037b3605629db58280cf06941bb2a6d85a102e
Carson <carsontwj@gmail.com>
2023-04-03 12:32:05 -0400

    Create module to return task element for the DOM

---
+++ src/js-modules/task-dom.js
@@ -0,0 +1,44 @@
+// function that returns a task for the DOM
+export default function createTask (title, date) {
+    //create default div to hold all the info
+    const taskDiv = document.createElement('div')
+    taskDiv.classList.add('task')
+    taskDiv.classList.add('task-span')
+
+    //create two halves for the div 
+    const taskStart = document.createElement('div')
+    const taskEnd = document.createElement('div')
+    taskStart.classList.add('task-start')
+    taskEnd.classList.add('task-end')
+
+    //create button to complete task
+    const checkButton = document.createElement('span')
+    checkButton.classList.add('check-button')
+
+    //create div to hold title of task
+    const taskTitle = document.createElement('div')
+    taskTitle.classList.add('task-title')
+    taskTitle.textContent = title
+
+    //create div to hold the date
+    const taskDate = document.createElement('div')
+    taskDate.classList.add('task-date')
+    taskDate.textContent = '1/2/2023'//TO DO
+
+    //create the edit button/img
+    const editButton = document.createElement('img')
+    editButton.src = 'images/edit.svg'
+    editButton.setAttribute('alt', 'edit button')
+
+    // Append all elements together
+    taskEnd.appendChild(taskDate)
+    taskEnd.appendChild(editButton)
+
+    taskStart.appendChild(checkButton)
+    taskStart.appendChild(taskTitle)
+
+    taskDiv.appendChild(taskStart)
+    taskDiv.appendChild(taskEnd)
+
+    return taskDiv
+}
