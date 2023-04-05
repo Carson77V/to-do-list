3a6f8dc2b8eaf308b243278e689b58ce3723f026
Carson <carsontwj@gmail.com>
2023-04-05 08:56:53 -0400

    Create function to remove all tasks in the DOM

--- src/js-modules/task-control.js
+++ src/js-modules/task-control.js
@@ -6,9 +6,21 @@ import createTaskEdit from './task-edit-dom.js'
 let allTasks = []
 
 export default function renderTasks (menuSelected) {
+    // first remove the current tasks
+    removeTasks()
     // select the add Task
     const tasksDiv = document.querySelector('#tasks')
     if (menuSelected = "All") {
         allTasks.forEach(createTask)
     }
+}
+
+// when renderTasks() is called, use this function to delete the current tasks
+function removeTasks () {
+    // create node lists of all current tasks in the DOM
+    const allTasks = document.querySelectorAll('.task-span')
+    // remove each task
+    allTasks.forEach((item) => {
+        item.remove()
+      });
 }
